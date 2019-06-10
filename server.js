cc10a1df9f99810d4d5fdb93dcb8a65b0365f6bb
Abusch419 <abusch419@gmail.com>
2019-06-10 18:03:53 -0400

    gets basic mvp working

--- server.js
+++ server.js
@@ -1,75 +1,119 @@
-
-// from homework instructions
-
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// mongoose.connect(MONGODB_URI)
-
-// dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-var cheerio = require("cheerio");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
+var cheerio = require("cheerio");
 
-// Axios request to pitchfork best albums of the week 
-axios.get("https://pitchfork.com/reviews/best/albums/").then(function(response) {
+// Require all models
+var db = require("./models");
 
-  // Load the HTML into cheerio and save it to a variable
-  var $ = cheerio.load(response.data);
-  var results = [];
-
-//  select the artist name and album name 
-  $(".artist-list").each(function(i, element) {
-
-    var artist = $(element).text()
-    var album = $(element).next(".review__title-album").text()
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      artist: artist,
-      album: album,
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
-
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Set up a static folder (public) for our web app
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-// Database configuration
-// Save the URL of our database as well as the name of our collection
-var databaseUrl = "albumsOfTheWeek";
-var collections = ["albums"];
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/pitchfork", { useNewUrlParser: true });
 
-// Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
+// Routes
 
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+// A GET route for scraping the echoJS website
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://pitchfork.com/reviews/best/albums/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $(".artist-list").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.artistName = $(this).text()
+      result.albumName = $(this).next(".review__title-album").text()
+        
+      // Create a new Article using the `result` object built from scraping
+      db.Album.create(result)
+        .then(function (dbAlbums) {
+          // View the added result in the console
+          console.log(dbAlbums);
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
 });
 
-app.get("/all", function(req, res) {
-    // Query: In our database, go to the animals collection, then "find" everything
-    db.albums.find({}, function(error, found) {
-      // Log any errors if the server encounters one
-      if (error) {
-        console.log(error);
-      }
-      // Otherwise, send the result of this query to the browser
-      else {
-        res.json(found);
-      }
-    });
-  });
+// Route for getting all Articles from the db
+app.get("/albums", function (req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Album.find({})
+    .then(function (dbAlbums) {
+      res.json(dbAlbums)
+    })
+    .catch(function (err) {
+      res.json(err)
+    })
+});
 
-// Set the app to listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/albums/:id", function (req, res) {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+  db.Album.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbAlbums) {
+      res.json(dbAlbums)
+    })
+    .catch(function (err) {
+      res.json(err)
+    })
+
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/albums/:id", function (req, res) {
+  // TODO
+  // ====
+  // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Album.findOneAndUpdate({_id: req.params.id}, { $set: {note: dbNote._id}}, { new: true })
+    })
+    .then(function (dbAlbum) {
+      res.json(dbAlbum)
+    })
+    .catch(function(err) {
+      res.json(err)
+    })
+});
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
 });
