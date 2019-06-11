1316e752bd00c31fca5badbf07d51afc05785f32
Abusch419 <abusch419@gmail.com>
2019-06-10 23:55:58 -0400

    adds spotify login and search functionality

--- public/app.js
+++ public/app.js
@@ -14,15 +14,19 @@ $.getJSON("/albums", function (data) {
     <img class="card-img-top" src="${data[i].albumArt}" alt="albumImage">
     <div class="card-body">
       <div class="row">
-        <a class="btn col-12 btn-default proj-btn" id="l-link"
+        <a class="btn col-12 btn-default review-btn" id="l-link"
           href="https://pitchfork.com${data[i].reviewLink}">Read Review</a>
-        <a class="btn col-12 btn-default proj-btn" id="r-link"
-          href="#">Create Spotify Playlist</a>
+        <a class="btn col-12 btn-default spotify-btn" id="r-link" data-name="${data[i].albumName}">Listen In Spotify</a>
       </div>
     </div>
   </div>`
     $("#albums").append(albumCard);
   }
+  $(document).on("click", ".spotify-btn", function (e) {
+    e.preventDefault()
+    selectedAlbum = $(this).data()
+    getAlbumUri(selectedAlbum, access_token);
+  })
 });
 
 
@@ -89,3 +93,124 @@ $(document).on("click", "#savenote", function () {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
+
+
+// spotify===========
+
+//////////////// Bearer Token //////////////////////////////////////////////////////////////
+
+let access_token;
+
+(function () {
+  var stateKey = "spotify_auth_state";
+  /**
+   * Obtains parameters from the hash of the URL
+   * @return Object
+   */
+  function getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  /**
+   * Generates a random string containing numbers and letters
+   * @param  {number} length The length of the string
+   * @return {string} The generated string
+   */
+  function generateRandomString(length) {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+  var userProfileSource = document.getElementById("user-profile-template")
+    .innerHTML,
+    userProfileTemplate = Handlebars.compile(userProfileSource),
+    userProfilePlaceholder = document.getElementById("user-profile");
+  (oauthSource = document.getElementById("oauth-template").innerHTML),
+    (oauthTemplate = Handlebars.compile(oauthSource)),
+    (oauthPlaceholder = document.getElementById("oauth"));
+  var params = getHashParams();
+  (access_token = params.access_token),
+    (state = params.state),
+    (storedState = localStorage.getItem(stateKey));
+  if (access_token && (state == null || state !== storedState)) {
+    alert("There was an error during the authentication");
+  } else {
+    //   localStorage.removeItem(stateKey);
+    if (access_token) {
+      $.ajax({
+        url: "https://api.spotify.com/v1/me",
+        headers: {
+          Authorization: "Bearer " + access_token
+        },
+        success: function (response) {
+          // userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+          $("#login").hide();
+          $("#sign-in").hide();
+          $(".album-container").show();
+        }
+      });
+    } else {
+      $("#login").show();
+      $("#loggedin").hide();
+    }
+    document.getElementById("login-button").addEventListener(
+      "click",
+      function (e) {
+        e.preventDefault();
+
+        var client_id = "68383a582d2344cdbd6623f1a47e797d"; // Your client id
+        var redirect_uri = "http://localhost:3000/"
+        var state = generateRandomString(16);
+        localStorage.setItem(stateKey, state);
+        var scope = "user-read-private user-read-email playlist-modify";
+        var url = "https://accounts.spotify.com/authorize";
+        url += "?response_type=token";
+        url += "&client_id=" + encodeURIComponent(client_id);
+        url += "&scope=" + encodeURIComponent(scope);
+        url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+        url += "&state=" + encodeURIComponent(state);
+
+        window.location = url;
+      },
+      false
+    );
+    // hiding from DOM until user is logged in to spotify
+    $("#user-profile").hide();
+    $(".album-container").hide();
+  }
+})();
+
+
+function getAlbumUri(searchParams, token) {
+  $.ajax({
+    url:
+      "https://api.spotify.com/v1/search?q=" +
+      searchParams +
+      "&" +
+      "type=album",
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token
+    },
+
+  })
+    .then(function (response) {
+      console.log(response.albums.items[0].uri)
+    })
+
+
+}
+
+
+
